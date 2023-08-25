faecab69c9e1828cf8c4845e71b6d64e2a961fe5
Cat P <caparker723@gmail.com>
2023-08-24 23:01:36 -0500

    Final commit

---
+++ script.js
@@ -0,0 +1,67 @@
+// Assignment code here
+
+var charLength = 8;
+var choiceArr = [];
+
+var specialCharArr = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","{","}","[","]","|",";","/","?","<",">",".",",",];
+var lowerCaseArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+var upperCaseArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+var numberArr = ["0","1","2","3","4","5","6","7","8","9",];
+
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomIndex];
+  }
+  return password;
+}
+
+function getPrompts() {
+  characterLength = parseInt(prompt("How many characters do you want your password to be? (8 minimum, 128 maximum)"));
+
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Character Length has to be a number from 8 - 128. Please try again.");
+    return false;
+  }
+
+  if (confirm("Would you like lowercase letters in your password?")) {
+    choiceArr = choiceArr.concat(lowerCaseArr);
+}
+
+  if (confirm("Would you like uppercase letters in your password?")) {
+   choiceArr = choiceArr.concat(upperCaseArr);
+}
+
+  if (confirm("Would you like special characters in your password?")) {
+    choiceArr = choiceArr.concat(specialCharArr);
+}
+
+  if (confirm("Would you like numbers in your password?")) {
+    choiceArr = choiceArr.concat(numberArr);
+}
+  return true;
+
+}
+
+// Get references to the #generate element
+
+// Write password to the #password input
+function writePassword() {
+    var correctPrompts = getPrompts();
+    var passwordText = document.querySelector("#password");
+    if(correctPrompts) {
+      var newPassword = generatePassword();
+
+      passwordText.value = newPassword;
+    } else {
+      passwordText.value = "";
+      
+    }
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
